faa86f0e688caad2478b91ee5d883196e3bb41de
xjzhaang <xingjian.zhang@polytechnique.edu>
2021-03-05 02:51:06 +0100

    fceri meanings

--- variables_parser.py
+++ variables_parser.py
@@ -1,3 +1,4 @@
+import copy
 def read_variables():
     #to 1479
     n = 0
@@ -6,6 +7,7 @@ def read_variables():
     translations = {"S0": "IgE", "S1": "Lyn", "S2": "Syk", "S3": "r"}
     for l in lines:
         n += 1
+        #print(n)
         substrates = l.split("->")[0].strip()
         if " + " in substrates:
             sub1 = substrates.split(" + ")[0]
@@ -26,81 +28,148 @@ def read_variables():
         if two_sub:
             if sub1 in translations and sub2 in translations:
                 if rate == "2.0 * pkp1":
-                    res_meaning = translations[sub1] + ", " + translations[sub2]
+                    if type(translations[sub1]) != list and type(translations[sub2]) != list:
+                        res_meaning = [translations[sub1], translations[sub2]]
+                    else:
+                        if type(translations[sub1]) != list and type(translations[sub2]) == list:
+                            res_meaning = copy.deepcopy(translations[sub2])
+                            res_meaning.append(translations[sub1])
+                        if type(translations[sub2]) != list and type(translations[sub1]) == list:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            res_meaning.append(translations[sub2])
+                        if type(translations[sub1]) == list and type(translations[sub2]) == list:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            for item in translations[sub2]:
+                                res_meaning.append(item)
                     if res_sub not in translations:
                         translations[res_sub] = res_meaning
                 if rate == "pkp2":
-                    res_meaning = translations[sub1] + ", " + translations[sub2]
+                    if type(translations[sub1]) != list and type(translations[sub2]) != list:
+                        res_meaning = [translations[sub1], translations[sub2]]
+                    else:
+                        if type(translations[sub1]) != list and type(translations[sub2]) == list:
+                            res_meaning = copy.deepcopy(translations[sub2])
+                            res_meaning.append(translations[sub1])
+                        if type(translations[sub2]) != list and type(translations[sub1]) == list:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            res_meaning.append(translations[sub2])
+                        if type(translations[sub1]) == list and type(translations[sub2]) == list:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            for item in translations[sub2]:
+                                res_meaning.append(item)
                     if res_sub not in translations:
                         translations[res_sub] = res_meaning
                 if rate == "pkpL":
                     if translations[sub1] == "Lyn":
-                        res_meaning = translations[sub2] + ", cLyn"
-                        if res_sub not in translations:
-                            translations[res_sub] = res_meaning
+                        if type(translations[sub2]) != list:
+                            res_meaning = [translations[sub2], "cLyn"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub2])
+                            res_meaning.append("cLyn")
                     else:
-                        res_meaning = translations[sub1] + ", cLyn"
-                        if res_sub not in translations:
-                            translations[res_sub] = res_meaning
+                        if type(translations[sub1]) != list:
+                            res_meaning = [translations[sub1], "cLyn"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            res_meaning.append("cLyn")
+                    if res_sub not in translations:
+                        translations[res_sub] = res_meaning
                 if rate == "pkpLs":
                     if translations[sub1] == "Lyn":
-                        res_meaning = translations[sub2] + ", SH2Lyn"
-                        if res_sub not in translations:
-                            translations[res_sub] = res_meaning
+                        if type(translations[sub2]) != list:
+                            res_meaning = [translations[sub2], "SH2Lyn"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub2])
+                            res_meaning.append("SH2Lyn")
                     else:
-                        res_meaning = translations[sub1] + ", SH2Lyn"
-                        if res_sub not in translations:
-                            translations[res_sub] = res_meaning
+                        if type(translations[sub1]) != list:
+                            res_meaning = [translations[sub1], "SH2Lyn"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            res_meaning.append("SH2Lyn")
+                    if res_sub not in translations:
+                        translations[res_sub] = res_meaning
                 if rate == "pkpS":
-                    res_meaning = translations[sub1] + ", " + translations[sub2]
+                    if translations[sub1] == "Syk":
+                        if type(translations[sub2]) != list:
+                            res_meaning = [translations[sub2], "Syk"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub2])
+                            res_meaning.append("Syk")
+                    else:
+                        if type(translations[sub1]) != list:
+                            res_meaning = [translations[sub1], "Syk"]
+                        else:
+                            res_meaning = copy.deepcopy(translations[sub1])
+                            res_meaning.append("Syk")
                     if res_sub not in translations:
                         translations[res_sub] = res_meaning
         if not two_res_sub:
             if sub1 in translations and sub2 in translations:
                 if rate == "ppLb":
-                    res_meaning = translations[sub1] + ", cbetaP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("betaP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppLg":
-                    res_meaning = translations[sub1] + ", cgammaP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("gammaP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppLbs":
-                    res_meaning = translations[sub1] + ", SH2betaP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("betaP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppLgs":
-                    res_meaning = translations[sub1] + ", SH2gammaP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("gammaP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppLS":
-                    res_meaning = translations[sub1] + ", cSykP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("cSykP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppLSs":
-                    res_meaning = translations[sub1] + ", SH2SykP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("SH2SykP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppSS":
-                    res_meaning = translations[sub1] + ", NotLoopSykP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("NoLoopSykP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
                 if rate == "ppSSs":
-                    res_meaning = translations[sub1] + ", LoopSykP"
+                    res_meaning = copy.deepcopy(translations[sub1])
+                    res_meaning.append("LoopSykP")
                     if res_sub1 not in translations:
                         translations[res_sub1] = res_meaning
         if two_res_sub:
             if sub1 in translations:
                 if rate == "ppkm1":
-                    res_meaning_1 = translations[sub1].replace(", r", "")
-                    res_meaning_2 = "r"
-                    if res_sub1 not in translations:
-                        translations[res_sub1] = res_meaning_1
+                    res_meaning_2 = copy.deepcopy(translations[sub1])
+                    for item in translations[res_sub1]:
+                        res_meaning_2.remove(item)
                     if res_sub2 not in translations:
                         translations[res_sub2] = res_meaning_2
                 if rate == "pkm2":
-                    res_meaning_2 = translations[sub1].replace(translations[res_sub1] + ", ", "")
+                    res_meaning_2 = copy.deepcopy(translations[sub1])
+                    print(res_meaning_2)
+                    print(translations[res_sub1])
+                    print(n)
+                    for item in translations[res_sub1]:
+                        res_meaning_2.remove(item)
                     if res_sub2 not in translations:
                         translations[res_sub2] = res_meaning_2
+                        #print(translations[res_sub2])
     print(translations)
+    return translations
+
 read_variables()
+with open('fceri_meanings','w') as afile:
+    translations = read_variables()
+    #print(translations["S4"])
+    list_of_strings = [ f'{key} : {translations[key]}' for key in translations]
+    [afile.write(f'{st}\n') for st in list_of_strings ]
