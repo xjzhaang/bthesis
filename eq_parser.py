6f19d3313217a28c552e23983ea350f35fe87bfa
pogudingleb <pogudin.gleb@gmail.com>
2021-03-22 13:01:54 +0300

    Eq parser code streamlining

--- eq_parser.py
+++ eq_parser.py
@@ -1,5 +1,6 @@
 from natsort import natsorted
-import copy 
+import copy
+import re
 
 #We first parse the meanings of bngl into a dictionary
 def parse_fceri_meanings():
@@ -27,101 +28,71 @@ def parse_eq():
         var2 = l.split(" = ")[1].strip().split(" + ")[1].strip()
         var = names_dict[var2]
         elements = var.split(".")
-        
+
+        # separateing the receptors
+        non_receptors = []
         for ele in elements:
             if "Rec(a!1" in ele:
                 receptor1.append(ele)
-            if "Rec(a!2" in ele:
+            elif "Rec(a!2" in ele:
                 receptor2.append(ele)
+            else:
+                non_receptors.append(ele)
+        elements = non_receptors
+
+        # splitting the stuff between the receptors
+        connection_re = r'!\d'
         for ele in elements:
-            if  "tSH2!6" in ele:
-                if "g~pY!6" in receptor1[0]:
+            c = re.search(connection_re, ele)
+            if c is not None:
+                if receptor1[0].find(c[0]) != -1:
                     receptor1.append(ele)
-                if "g~pY!6" in receptor2[0]:
+                elif receptor2[0].find(c[0]) != -1:
                     receptor2.append(ele)
-            if  "tSH2!5" in ele:
-                if "g~pY!5" in receptor1[0]:
-                    receptor1.append(ele)
-                if "g~pY!5" in receptor2[0]:
-                    receptor2.append(ele)
-            if  "tSH2!4" in ele:
-                if "g~pY!4" in receptor1[0]:
-                    receptor1.append(ele)
-                if "g~pY!4" in receptor2[0]:
-                    receptor2.append(ele)
-            if  "tSH2!3" in ele:
-                if "g~pY!3" in receptor1[0]:
-                    receptor1.append(ele)
-                if "g~pY!3" in receptor2[0]:
-                    receptor2.append(ele)
-        for ele in elements:
-            if "Lyn(SH2!3" in ele:
-                if "b~pY!3" in receptor1[0]:
-                    receptor1.append(ele)
-                if "b~pY!3" in receptor2[0]:
-                    receptor2.append(ele)
-            if "Lyn(SH2!4" in ele:
-                if "b~pY!4" in receptor1[0]:
-                    receptor1.append(ele)
-                if "b~pY!4" in receptor2[0]:
-                    receptor2.append(ele)
-            if "U!3" in ele:
-                if "b~Y!3" in receptor1[0]:
-                    receptor1.append(ele)
-                if "b~Y!3" in receptor2[0]:
-                    receptor2.append(ele)
-            if "U!4" in ele:
-                if "b~Y!4" in receptor1[0]:
-                    receptor1.append(ele)
-                if "b~Y!4" in receptor2[0]:
-                    receptor2.append(ele)
-        if "b~Y" in receptor1[0]:
-            receptor1[0] = "Rec(b)"
-        if "b~pY" in receptor1[0]: 
-            receptor1[0] = "Rec(bp)"
-        if "b~Y" in receptor2[0]:
-            receptor2[0] = "Rec(b)"
-        if "b~pY" in receptor2[0]: 
-            receptor2[0] = "Rec(bp)"
+                else:
+                    print(f"No owner found for {ele}")
+            else:
+                print(f"Unconnected element {ele}")
 
-        if "Syk(a~Y,l~Y" in receptor1[1]:
-            receptor1[1] = "Syk(a,l)"
-        if "Syk(a~Y,l~Y" in receptor2[1]:
-            receptor2[1] = "Syk(a,l)"
+        # sorting elements: making Syk going before Lyn
+        def sort_rec(receptor):
+            if len(receptor) > 2 and receptor[1] < receptor[2]:
+                receptor[1], receptor[2] = receptor[2], receptor[1]
+        sort_rec(receptor1)
+        sort_rec(receptor2)
 
-        if "Syk(a~pY,l~Y" in receptor1[1]:
-            receptor1[1] = "Syk(ap,l)"
-        if "Syk(a~pY,l~Y" in receptor2[1]:
-            receptor2[1] = "Syk(ap,l)"
+        # translation/simplification
+        def subs_rec(receptor):
+            if "b~Y" in receptor[0]:
+                receptor[0] = "Rec(b)"
+            if "b~pY" in receptor[0]: 
+                receptor[0] = "Rec(bp)"
+        subs_rec(receptor1)
+        subs_rec(receptor2)
 
-        if "Syk(a~Y,l~pY" in receptor1[1]:
-            receptor1[1] = "Syk(a,lp)"
-        if "Syk(a~Y,l~pY" in receptor2[1]:
-            receptor2[1] = "Syk(a,lp)"
+        def subs_syk(receptor):
+            if "Syk(a~Y,l~Y" in receptor[1]:
+                receptor[1] = "Syk(a,l)"
+            if "Syk(a~pY,l~Y" in receptor[1]:
+                receptor[1] = "Syk(ap,l)"
+            if "Syk(a~Y,l~pY" in receptor[1]:
+                receptor[1] = "Syk(a,lp)"
+            if "Syk(a~pY,l~pY" in receptor[1]:
+                receptor[1] = "Syk(ap,lp)"
+        subs_syk(receptor1)
+        subs_syk(receptor2)
 
-        if "Syk(a~pY,l~pY" in receptor1[1]:
-            receptor1[1] = "Syk(ap,lp)"
-        if "Syk(a~pY,l~pY" in receptor2[1]:
-            receptor2[1] = "Syk(ap,lp)"
-        
-        if len(receptor1) == 3:
-            if "Lyn(SH2,U!3)" in receptor1[2]:
-                receptor1[2] = "Lyn(c)"
-            if "Lyn(SH2,U!4)" in receptor1[2]:
-                receptor1[2] = "Lyn(c)"
-            if "Lyn(SH2!3,U)" in receptor1[2]:
-                receptor1[2] = "Lyn(s)"
-            if "Lyn(SH2!4,U)" in receptor1[2]:
-                receptor1[2] = "Lyn(s)"
-        if len(receptor2) == 3:
-            if "Lyn(SH2,U!4)" in receptor2[2]:
-                receptor2[2] = "Lyn(c)"
-            if "Lyn(SH2,U!3)" in receptor2[2]:
-                receptor2[2] = "Lyn(c)"
-            if "Lyn(SH2!3,U)" in receptor2[2]:
-                receptor2[2] = "Lyn(s)"
-            if "Lyn(SH2!4,U)" in receptor2[2]:
-                receptor2[2] = "Lyn(s)"
+        def subs_lyn(receptor):
+            if len(receptor) == 3:
+                if re.search(r'Lyn\(SH2,U!\d\)', receptor[2]) is not None:
+                    receptor[2] = "Lyn(c)"
+                elif re.search(r'Lyn\(SH2!\d,U\)', receptor[2]) is not None:
+                    receptor[2] = "Lyn(s)"
+                else:
+                    print(f"Unrecognized Lyn: {receptor[2]}")
+        subs_lyn(receptor1)
+        subs_lyn(receptor2)
+
         eq_dict[name] = (receptor1,receptor2)
     return eq_dict
 
@@ -173,8 +144,12 @@ def compare_outputs():
                 list2.remove(item)
             if values[0] == item[1] and values[1] == item[0]:
                 list2.remove(item)
+    print(len(list2))
     print(list2)
+
 compare_outputs()
+
+exit()
 
 with open('fceri_meanings','w') as afile:
     names_dict = parse_fceri_meanings()
